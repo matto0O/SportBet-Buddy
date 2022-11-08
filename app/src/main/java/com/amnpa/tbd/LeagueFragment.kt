975a6355175c36c16c7f4a3e02c60375f3306143
Mateusz <mateusz.wozniak.0211@gmail.com>
2022-11-08 14:06:08 +0100

    Started development of a fragment related to the league system.

--- app/src/main/java/com/amnpa/tbd/LeagueFragment.kt
+++ app/src/main/java/com/amnpa/tbd/LeagueFragment.kt
@@ -5,13 +5,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class LeagueFragment : Fragment() {
+
+    private lateinit var textEnterCode:EditText
+    private lateinit var buttonJoin:Button
+    private lateinit var buttonCreate:Button
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_league, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_league, container, false)
+
+        textEnterCode = view.findViewById(R.id.editTextEnterCode)
+        buttonJoin = view.findViewById(R.id.buttonJoinLeague)
+        buttonCreate = view.findViewById(R.id.buttonCreateLeague)
+
+        buttonJoin.setOnClickListener{
+            if (textEnterCode.text.toString() == "")
+                Toast.makeText(context, "Invalid league code", Toast.LENGTH_SHORT).show()
+            else
+                Toast.makeText(context, "TODO: joined a league of code: "
+                        +textEnterCode.text.toString(), Toast.LENGTH_SHORT).show()
+        }
+
+        return view
     }
 }
