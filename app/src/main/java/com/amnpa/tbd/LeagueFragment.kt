ed966d52c42eb8560cc4a47da76dddd331937535
Mateusz <mateusz.wozniak.0211@gmail.com>
2022-11-09 14:54:03 +0100

    Completed a provisional league creation popup

--- app/src/main/java/com/amnpa/tbd/LeagueFragment.kt
+++ app/src/main/java/com/amnpa/tbd/LeagueFragment.kt
@@ -6,12 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ListView
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 
 class LeagueFragment : Fragment() {
 
@@ -50,6 +45,40 @@ class LeagueFragment : Fragment() {
             textLeagueName.text = selected.getName()
             listPlayers.adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1,
                 selected.orderedPlayers())
+
+            builder.setView(popup)
+            builder.create().show()
+        }
+
+        buttonCreate.setOnClickListener {
+            val builder = AlertDialog.Builder(context)
+            val popup = layoutInflater.inflate(R.layout.league_creator_popup, null)
+
+            val textLeagueName = popup.findViewById<EditText>(R.id.editTextLeagueName)
+            val buttonCommit = popup.findViewById<Button>(R.id.buttonCommitLeagueCreation)
+            val listEvents = popup.findViewById<ListView>(R.id.listViewChooseEvents)
+
+            val checkRowList = mutableListOf(CheckRow("Ekstraklasa"),
+                CheckRow("Liga Mistrzow"),
+                CheckRow("Mistrzostwa Swiata"),
+                CheckRow("NBA"))
+
+            val adapter = CheckRowAdapter(checkRowList, requireContext())
+            listEvents.adapter = adapter
+
+            listEvents.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
+                val checkRow: CheckRow = checkRowList[position]
+                checkRow.checked = !checkRow.checked
+                adapter.notifyDataSetChanged()
+            }
+
+            buttonCommit.setOnClickListener {
+                if (textLeagueName.text.toString().length < 3)
+                    Toast.makeText(context, "Invalid league name", Toast.LENGTH_SHORT).show()
+                else
+                    Toast.makeText(context, "Created ${textLeagueName.text}",
+                        Toast.LENGTH_SHORT).show()
+            }
 
             builder.setView(popup)
             builder.create().show()
