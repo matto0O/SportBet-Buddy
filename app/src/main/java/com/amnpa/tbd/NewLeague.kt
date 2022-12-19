e1eada11e3b97d1bdd758082e90c3158ce168d2a
Mateusz <mateusz.wozniak.0211@gmail.com>
2022-12-19 21:14:44 +0100

    Deleted redundant files

--- app/src/main/java/com/amnpa/tbd/NewLeague.kt
+++
@@ -1,8 +0,0 @@
-package com.amnpa.tbd
-
-data class NewLeague (val code:String, /*val name:String,*/ val leagueId:Int,
-                      val competitions: List<NewCompetition>, val players: List<Int>){
-    override fun toString(): String {
-        return "$code - $leagueId"
-    }
-}
