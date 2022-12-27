2e75c78a96f66cdd43142beb8a9d6c936a02a6a9
Mateusz <mateusz.wozniak.0211@gmail.com>
2022-12-27 16:38:51 +0100

    Combined settings, stats and user fragments into one. Implemented a new, animated bottom navigation.

--- app/src/main/java/com/amnpa/sbb/MainActivity.kt
+++ app/src/main/java/com/amnpa/sbb/MainActivity.kt
@@ -2,18 +2,55 @@ package com.amnpa.sbb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.findNavController
-import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.fragment.app.Fragment
+import com.amnpa.sbb.viewmodel.GamesFragment
+import com.amnpa.sbb.viewmodel.LeagueFragment
+import com.amnpa.sbb.viewmodel.StatsFragment
+import com.etebarian.meowbottomnavigation.MeowBottomNavigation
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
-        val navController = findNavController(R.id.fragmentContainerView)
+        val bottomNavigationView = findViewById<MeowBottomNavigation>(R.id.bottomNavigationView)
 
-        bottomNavigationView.setupWithNavController(navController)
+        bottomNavigationView.add(MeowBottomNavigation.Model(1, R.drawable.ic_user))
+        bottomNavigationView.add(MeowBottomNavigation.Model(2, R.drawable.ic_games))
+        bottomNavigationView.add(MeowBottomNavigation.Model(3, R.drawable.ic_league))
+
+        goToFragment(GamesFragment())
+
+        bottomNavigationView.setOnShowListener {
+            fun onShowItem() {
+                goToFragment(
+                    when(it.id){
+                        1 -> StatsFragment()
+                        2 -> GamesFragment()
+                        else -> LeagueFragment()
+                    }
+                )
+            }
+        }
+
+        bottomNavigationView.setOnClickMenuListener{
+            goToFragment(
+                when(it.id){
+                    1 -> StatsFragment()
+                    2 -> GamesFragment()
+                    else -> LeagueFragment()
+                }
+            )
+        }
+
+        bottomNavigationView.show(2, true)
+    }
+
+    private fun goToFragment(fragment: Fragment){
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragmentContainerView, fragment)
+            .commit()
     }
 }
